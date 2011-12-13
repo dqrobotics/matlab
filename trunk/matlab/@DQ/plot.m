dfb6ea82da0d21d98c383ce226309b562949ac16
bvadorno <bvadorno@4392d9c1-53a7-476a-8290-df5c8d09eb13>
2011-12-13 14:55:47 +0000

    Matlab plot function fixed

--- trunk/matlab/@DQ/plot.m
+++ trunk/matlab/@DQ/plot.m
@@ -1,17 +1,17 @@
-%plot(dq,OPTIONS) plots the dual quaternion dq. The following options are
-%available:
+% h = plot(dq,OPTIONS) plots the dual quaternion dq and returns the handle of the plot. 
+% The following options are available:
 %   'scale'
 %   'erase'
 % Ex.: plot(dq,'scale',5) will plot dq with the axis scaled by a factor of 5
 %      
-% plot(dq,'erase') will plot dq, but erasing previous values of dq. This is
-% useful for plotting frames in motion (if erase is not used, it will leave a trail)
-function dq=plot(dq,varargin)
+% plot(dq,'erase', h) will plot dq, but erasing previous values of dq. The parameter h
+% is the handle of the plot. This is useful for plotting frames in motion
+% (if erase is not used, it will leave a trail)
+function handle=plot(dq,varargin)
 
 if norm(dq) ~= 1
     error('Only unit dual quaternions can be plotted');
 end
-
 
 optargin = size(varargin,2);
 
@@ -19,23 +19,28 @@ scale = 1;
 
 if optargin > 0
     for j =1:optargin
-        my_string = num2str(cell2mat(varargin(1,j)));
-        if findstr(my_string,'era')
-           
-            if(dq.isplot)
-                dq.isplot = 0;
-                for i = 1:3                
-                    delete(dq.handle_axis{i});
-                    delete(dq.handle_text{i});
-                end 
+        
+        my_string = varargin(1,j);%num2str(cell2mat(varargin(1,j)));
+       
+        if(~isstruct(my_string{1}))
+            if strfind(my_string{1},'era')
+                handle_cell = varargin(1,j+1);  
+                handle = handle_cell{1};
+                    for i = 1:3   
+                        delete(handle.handle_axis{i});
+                        delete(handle.handle_text{i});
+                    end 
+                   
+            elseif strfind(my_string,'sca')
+                   scale = cell2mat(varargin(1,j+1));              
             end
-        elseif findstr(my_string,'sca')
-                scale = cell2mat(varargin(1,j+1));
         end
     end
 end
 
-    dq.isplot = 1;
+  
+    
+    
     % create unit vectors and rotate them by the quaternion part of dq.
     t1 = 1+DQ.E*[0, scale*0.5, 0, 0];
     t2 = 1+DQ.E*[0, 0, scale*0.5, 0];
@@ -53,21 +58,21 @@ end
     y_t = translation(y);
     z_t = translation(z);
     
-    dq.handle_axis{1} = plot3([0;x_t.q(2)]+dq_t.q(2), [0; x_t.q(3)]+dq_t.q(3), [0; x_t.q(4)]+dq_t.q(4), 'r','Linewidth',1,'erasemode', 'xor');
+    handle.handle_axis{1} = plot3([0;x_t.q(2)]+dq_t.q(2), [0; x_t.q(3)]+dq_t.q(3), [0; x_t.q(4)]+dq_t.q(4), 'r','Linewidth',1,'erasemode', 'xor');
     if(~ishold)
         hold on;
     end
     
-	dq.handle_text{1} = text(dq_t.q(2)+x_t.q(2), dq_t.q(3)+x_t.q(3), dq_t.q(4)+x_t.q(4), 'x');
-	set(dq.handle_text{1} , 'Color', 'k');
+	handle.handle_text{1} = text(dq_t.q(2)+x_t.q(2), dq_t.q(3)+x_t.q(3), dq_t.q(4)+x_t.q(4), 'x');
+	set(handle.handle_text{1} , 'Color', 'k');
 
-	dq.handle_axis{2} =plot3([0;y_t.q(2)]+dq_t.q(2), [0; y_t.q(3)]+dq_t.q(3), [0; y_t.q(4)]+dq_t.q(4), 'g','Linewidth',1,'erasemode', 'xor');
-	dq.handle_text{2}  = text(dq_t.q(2)+y_t.q(2), dq_t.q(3)+y_t.q(3), dq_t.q(4)+y_t.q(4), 'y');
-	set(dq.handle_text{2} , 'Color', 'k');
+	handle.handle_axis{2} =plot3([0;y_t.q(2)]+dq_t.q(2), [0; y_t.q(3)]+dq_t.q(3), [0; y_t.q(4)]+dq_t.q(4), 'g','Linewidth',1,'erasemode', 'xor');
+	handle.handle_text{2}  = text(dq_t.q(2)+y_t.q(2), dq_t.q(3)+y_t.q(3), dq_t.q(4)+y_t.q(4), 'y');
+	set(handle.handle_text{2} , 'Color', 'k');
 
-	dq.handle_axis{3} = plot3([0;z_t.q(2)]+dq_t.q(2), [0; z_t.q(3)]+dq_t.q(3), [0; z_t.q(4)]+dq_t.q(4), 'b','Linewidth',1,'erasemode', 'xor');
-	dq.handle_text{3}  = text(dq_t.q(2)+z_t.q(2), dq_t.q(3)+z_t.q(3), dq_t.q(4)+z_t.q(4), 'z');
-	set(dq.handle_text{3} , 'Color', 'k');
+	handle.handle_axis{3} = plot3([0;z_t.q(2)]+dq_t.q(2), [0; z_t.q(3)]+dq_t.q(3), [0; z_t.q(4)]+dq_t.q(4), 'b','Linewidth',1,'erasemode', 'xor');
+	handle.handle_text{3}  = text(dq_t.q(2)+z_t.q(2), dq_t.q(3)+z_t.q(3), dq_t.q(4)+z_t.q(4), 'z');
+	set(handle.handle_text{3} , 'Color', 'k');
     if(~old_ishold)
         hold off;
     end
