d893ff976da6de987b1b690c168e31a9b001ef31
bvadorno <bvadorno@4392d9c1-53a7-476a-8290-df5c8d09eb13>
2012-10-18 21:08:31 +0000

    Fixed a bug in @DQ_kinematics/plot

--- @DQ/pinv.m
+++ @DQ/pinv.m
@@ -1,4 +1,7 @@
+%PINV(dq) returns the inverse of dq under the decompositional
+%multiplication (i.e., the group operation of CMI(3))
 function ret = pinv(dq)
-    temp = dq .* dq';    
+    dq = DQ(dq);
+    temp = tplus(dq)*tplus(dq');    
     ret = temp'*dq';
 end
