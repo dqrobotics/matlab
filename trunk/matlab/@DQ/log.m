abfcc4df42662ba1ba7368c1c9bc7140793cd95e
bvadorno <bvadorno@4392d9c1-53a7-476a-8290-df5c8d09eb13>
2014-04-14 13:56:57 +0000

    Log function updated (it does not use depecrated functions anymore)

--- trunk/matlab/@DQ/log.m
+++ trunk/matlab/@DQ/log.m
@@ -1,13 +1,13 @@
-% The function log(dq) returns the logarithm of the dual quaternion dq
-function ret = log(dq)
-dq = DQ(dq);
+% LOG(x) returns the logarithm of the dual quaternion x
+function ret = log(x)
+x = DQ(x);
 
-if norm(dq) ~= 1
+if norm(x) ~= 1
     error('The log function is currently defined only for unit dual quaternions.');
 end
 
-primary = mod(real(dq.theta/2),pi)*dq.rotation_axis;
-dual = translation(dq)*0.5;
+primary = mod(real(x.rotation_angle/2),pi)*x.rotation_axis;
+dual = translation(x)*0.5;
 
 ret = primary + DQ.E* dual;
 
