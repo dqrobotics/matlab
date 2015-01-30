cf687cc626c4bacd9fd25ccfd80813ced60c3472
bvadorno <bvadorno@4392d9c1-53a7-476a-8290-df5c8d09eb13>
2015-01-30 13:48:46 +0000

    List of changes
    matlab/DQ_robots
    	- Added DQ_WAM.m
    	- Added DQ_AX18.m
    	- Added DQ_COMAU.m
    
    
    matlab/examples/
    	- AX18.m was renamed to AX18_kinematic_control.m
    	- two_kukas_manipulating_broom was removed.
    
    matlab/examples/kuka_kinematic_control.m:
    	- Instead of explicitly declaring the DH parameters, now we're using the function DQ_KUKA to do it.
    
    matlab/examples/WAM_kinematic_control.m:
    	- Instead of explicitly declaring the DH parameters, now we're using the function DQ_WAM to do it.
    
    matlab/examples/AX18_kinematic_control.m:
    	- Instead of explicitly declaring the DH parameters, now we're using the function DQ_AX18 to do it.
    	- The whole file was cleaned up and organized.
    
    matlab/examples/comau_kinematic_control.m:
    	- Instead of explicitly declaring the DH parameters, now we're using the function DQ_COMAU to do it.
    	- The file was organised.
    
    matlab/@DQ/times.m
    	- tplus() was replaced by T();
    
    matlab/examples/cdts_broom_kuka.m:
    	- The file was organized.

--- examples/WAM_kinematic_control.m
+++ examples/WAM_kinematic_control.m
@@ -3,23 +3,13 @@ clear all;
 clear classes;
 clc;
 
-%Standard D-H of WAM
-wam_DH_theta=[0, 0, 0, 0, 0, 0, 0];
-wam_DH_d = [0, 0, 0.55, 0, 0.3, 0, 0.0609];
-wam_DH_a = [0, 0, 0.045, -0.045, 0, 0, 0];
-wam_DH_alpha =  pi*[-0.5, 0.5, -0.5, 0.5, -0.5, 0.5, 0];
-wam_DH_matrix = [wam_DH_theta;
-                  wam_DH_d;
-                  wam_DH_a;
-                  wam_DH_alpha];
-                              
-wam = DQ_kinematics(wam_DH_matrix,'standard');
+%Create a new DQ_kinematics object with the WAM arm standard Denavit-Hartenberg parameters                              
+wam = DQ_WAM;
 
 %Initial configuration
-thetastart =[-2.65    -0.604    -0.46    2.68        1.35         1.61         -3.03]';
+theta =[-2.65    -0.604    -0.46    2.68        1.35         1.61         -3.03]';
 %Final configuration
 thetad = [-2.0    -0.304    -0.3    2.3        1.0         1.0         -2.03]';
-theta = thetastart;
 
 epsilon = 0.001; %The error must be bellow this value in order to stop the robot
 gain = 0.1; %Gain of the controllers
@@ -35,9 +25,8 @@ view(-0,0)
 hold on;
 
 plot(wam, theta);
-pause
 
-fprintf('Performing standard kinematic control using dual quaternion coordinates');
+disp('Performing standard kinematic control using dual quaternion coordinates');
 xm = wam.fkm(theta);
 error = epsilon+1;
 while norm(error) > epsilon
@@ -46,5 +35,6 @@ while norm(error) > epsilon
     error = vec8(xd-xm);
     theta = theta+pinv(jacob)*gain*error;
     plot(wam, theta');    
+    drawnow;
 end
 
