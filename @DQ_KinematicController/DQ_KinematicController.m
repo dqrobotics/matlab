042e220ecf6d22428dc5d924d465bf23e8d86c2d
bvadorno <bvadorno@4392d9c1-53a7-476a-8290-df5c8d09eb13>
2017-07-24 15:34:58 +0000

    1) Implementation of the parsimonious controller in class DQ_KinematicController; 2) Improved the corresponding example file.

--- @DQ_KinematicController/DQ_KinematicController.m
+++ @DQ_KinematicController/DQ_KinematicController.m
@@ -9,13 +9,12 @@
 %       pseudoinverse_pose_controller
 %
 % All controllers are based on the following data structure:
-        % Minimal structure (i.e., all controllers must have the following
-        % information):
-        %     controller_variables.q: current robot configuration;
-        %     controller_variables.K: current gain matrix;
-        %     controller_variables.xd: current desired end-effector pose;
+%           Minimal structure (i.e., all controllers must have the following
+%           information):
+%               controller_variables.q: current robot configuration;
+%               controller_variables.K: current gain matrix;
+%               controller_variables.xd: current desired end-effector pose;
         
-
 % (C) Copyright 2017 DQ Robotics Developers
 % 
 % This file is part of DQ Robotics.
@@ -121,6 +120,97 @@ classdef DQ_KinematicController
             if nargout == 2
                 varargout{1} = e;
             end
+        end
+        
+        function [u, varargout] = parsimonious_pose_controller(obj,controller_variables) 
+        % parsimonious_pose_controller    Parsimonious pose controller based on the 
+        % paper 
+        % [1] V. M. Goncalves, P. Fraisse, A. Crosnier, and B. V. Adorno, "Parsimonious Kinematic Control of 
+        % Highly Redundant Robots," IEEE Robot. Autom. Lett., vol. 1, no. 1, pp. 65?72, Jan. 2016.
+        %
+        % [u,e,fval] = parsimonious_pose_controller(controller_variables)
+        % returns the control input u (joint velocities), the current error
+        % e = vec(xd - x), where xd is the desired pose and x is the
+        % current one, and fval is the value of the objective function at
+        % u.
+        %
+        % [u,e] = parsimonious_pose_controller(controller_variables)
+        % returns the control input u (joint velocities) and the current error
+        % e = vec(xd - x), where xd is the desired pose and x is the current one.
+        %
+        % u = parsimonious_pose_controller(controller_variables) returns
+        % only the control input u.
+        %
+        % This controller needs the minimal structure for controller
+        % variables (see help DQ_KinematicController) plus the additional equality and
+        % inequality constraints.
+        % TODO: This is just the basic implementation, so for the moment
+        % there is no possibility of adding additional inequality or
+        % equality constraints.
+        % The optimization problem is slightly different from the one of
+        % [1], in the sense that the convergence parameter (eta) is implemented
+        % here as a positive definite matrix, not just a scalar. 
+        
+            % DOFS
+            ROBOT_DOFS   = length(obj.robot.q);
+            % The numbers of the next variable are related to Problems 6 and 7 of
+            % Gon?alves et al. (2016). More specifically, g = [qp_dot, qn_dot, y, za,
+            % zc], where q_dot = qp_dot - qn_dot, y comes from Problem 6 and za (it has 
+            % the same dimension of y) and zc are the slack variables.
+            PROBLEM_DOFS = ROBOT_DOFS*2+8*2+1; 
+             % Ones
+            ones_rdof    = ones(ROBOT_DOFS,1); % ones vector related to the joint space
+            ones_e       = ones(8,1); % ones vector related to the task space
+            % Zeros
+            zeros_rdof   = zeros(ROBOT_DOFS,1); % zeroes vector related to the joint space (Do not confuse this with a slack variable)
+            zeros_e      = zeros(8,1); % zeroes vector related to the task space
+            zeros_p      = zeros(PROBLEM_DOFS,1); % zeroes vector related to the problem
+            % Identity matrices
+            I_rdof  = eye(ROBOT_DOFS); 
+            I_e     = eye(8); 
+            I_p     = eye(PROBLEM_DOFS); 
+       
+        
+            J = obj.robot.jacobian(controller_variables.q);
+            x = obj.robot.fkm(controller_variables.q);
+            % The error is inverted with respect to the classic pseudoinverse
+            % controller. Of course, we could change the sign of the
+            % classic pseudoinverse error and also change the sign of the
+            % control input as well in that case. 
+            e = vec8(x - controller_variables.xd);
+            
+            %% Linear programming definitions. See [1-2]
+            f   = [-ones_e'*J ones_e'*J 2*ones_e' zeros_e' 0];     % [-1'*J 1'*J 2*1' 0' 0];
+            % A and b are used to enforce the constraint g >=0 (Problem 7 of Gon?alves et al., 2016)
+            A   = [-I_p];
+            b   = [zeros_p];
+            % Aeq and Beq are used to enforce the equality constraint of Problem 7
+            % of Gon?alves et al., 2016.
+            Aeq = [[J;ones_rdof'], [-J;ones_rdof'], [-I_e;zeros_e'] , [I_e;zeros_e'] , [zeros_e;1]];   % [J;1' -J;1' -I;0' I;0' 0;1]
+            beq = [-controller_variables.K*e;controller_variables.beta*norm(e,1)];
+    
+            %% Linear Programming    
+            % The syntax of linprog has changed starting from version 9.1. Thus we 
+            % write code to both recent and older versions in order to guarantee
+            % backward compability.    
+            if verLessThan('matlab','8.4')        
+                options = optimoptions('linprog','Algorithm','simplex','Display','off');
+                [g,fval] = linprog(f,A,b,Aeq,beq,[],[],[],options);      
+            else
+            % Linear Programming (The default algorithm in matlab is the dual-simplex)
+                options = optimoptions('linprog','Algorithm','dual-simplex');
+                g = linprog(f,[],[],Aeq,beq,zeros(PROBLEM_DOFS,1),[],options); % g = [QP' QN' Y ZB ZC]
+            end
+            QP = g(1:ROBOT_DOFS);
+            QN = g(ROBOT_DOFS+1:ROBOT_DOFS*2);
+            u = QP-QN; % u = QP-QN
+            
+            if nargout >= 2
+                varargout{1} = e;
+                if nargout == 3 % Returns the value of the objective function (See Eq. (7) of [1]).                      
+                    varargout{2} = fval - ones_e'*controller_variables.K * e;
+                end
+            end            
         end
     end
 end
--- examples/example_dq_kinematic_control.m
+++ examples/example_dq_kinematic_control.m
@@ -1,3 +1,26 @@
+% (C) Copyright 2017 DQ Robotics Developers
+% 
+% This file is part of DQ Robotics.
+% 
+%     DQ Robotics is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU Lesser General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     DQ Robotics is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU Lesser General Public License for more details.
+% 
+%     You should have received a copy of the GNU Lesser General Public License
+%     along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
+%
+% DQ Robotics website: dqrobotics.sourceforge.net
+%
+% Contributors to this file:
+%     Bruno Vihena Adorno - adorno@ufmg.br
+
+
 close all;
 clear all;
 clear classes;
@@ -20,6 +43,10 @@ controller_variables.K = 0.1; % Controller gain
 controller_variables.q = q; % Current robot configuration 
 controller_variables.xd = xd; % Desired end-effector pose
 controller_variables.lambda = 0.09; % This is the damping factor for the damped_pseudo_inverse controller;
+% This is the variable used in the parsimonious controller to enforce null 
+% joints velocities when the error goes to zero
+controller_variables.beta = 1; 
+
 % Stop criterion. The error must be bellow this value in order to stop the 
 % robot.
 epsilon = 0.001; 
@@ -31,15 +58,59 @@ plot(kuka, q, 'nobase');
 view(-0,0);
 grid off;
 
+
 % Let's control our robot
-e = epsilon + 1;
-while norm(e) > epsilon    
-    [u,e] = kinematic_controller.damped_pseudoinverse_pose_controller(controller_variables); 
-    controller_variables.q = controller_variables.q + u; %integrates the control input and update the structure controller_variables
+controller_type = 'pseudoinverse';
+while ~strcmp(controller_type,'none')
     
-    % Visualization
-    plot(kuka, controller_variables.q);  
-    drawnow;
+    %For all controllers, the robot starts at the same configuration
+    controller_variables.q = [0; 0.3770; 0.1257; -0.5655; 0; 0; 0];
+    ii = 1;
+    e = epsilon + 1;
+    
+    switch controller_type
+        case 'pseudoinverse'
+            tic 
+            while norm(e) > epsilon 
+                [u,e] = kinematic_controller.pseudoinverse_pose_controller(controller_variables);                 
+                controller_variables.q = controller_variables.q + u; %integrates the control input and update the structure controller_variables
+                % Visualization
+                plot(kuka, controller_variables.q);  
+                drawnow;
+                ii = ii + 1;
+            end
+            toc
+            fprintf('\n Number of iterations for the pseudoinverse controller: %f\n', ii);
+            controller_type = 'damped_pseudoinverse';
+            
+        case 'damped_pseudoinverse'
+            tic 
+            while norm(e) > epsilon 
+                [u,e] = kinematic_controller.damped_pseudoinverse_pose_controller(controller_variables);                 
+                controller_variables.q = controller_variables.q + u; %integrates the control input and update the structure controller_variables
+                % Visualization
+                plot(kuka, controller_variables.q);  
+                drawnow;
+                ii = ii + 1;
+            end
+            toc
+            fprintf('\n Number of iterations for the damped pseudoinverse controller: %f\n', ii);
+            controller_type = 'parsimonious';
+            
+        case 'parsimonious'
+            tic 
+            while norm(e) > epsilon 
+                [u,e] = kinematic_controller.parsimonious_pose_controller(controller_variables);                 
+                controller_variables.q = controller_variables.q + u; %integrates the control input and update the structure controller_variables
+                % Visualization
+                plot(kuka, controller_variables.q);  
+                drawnow;
+                ii = ii + 1;
+            end
+            toc
+            fprintf('\n Number of iterations for the parsimonious controller: %f\n', ii);
+            controller_type = 'none';
+    end
 end
 
 
