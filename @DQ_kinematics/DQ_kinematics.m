4912869690b1126d3500df275f07a31e5cf01660
Bruno Vilhena Adorno <adorno@ufmg.br>
2019-03-06 16:43:29 -0300

    Main changes:
    1) Added the funcionality to retrieve the Jacobian matrix up to the ith link.
    2) Added an internal function: raw_jacobian. It does not take into consideration
       and end-effector constant displacements.
    3) Improved the documentation.

--- @DQ_kinematics/DQ_kinematics.m
+++ @DQ_kinematics/DQ_kinematics.m
@@ -1,14 +1,16 @@
 % CLASS DQ_kinematics
-% Usage: robot_kine = DQ_kinematics(A,convention), where:
-% A is a 4 x n matrix containing the Denavit-Hartenberg parameters (n is the number of links)
+% Usage: robot_kine = DQ_kinematics(A,convention)
+% - 'A' is a 4 x n matrix containing the Denavit-Hartenberg parameters
+%   (n is the number of links)
 %    A = [theta1 ... thetan;
 %            d1  ...   dn;
 %            a1  ...   an;
 %         alpha1 ... alphan]
-% convention is the convention used for the D-H parameters. Accepted values are
-% 'standard' and 'modified'
-% 
-% Please note that the theta_i is already the offset for the i-th joint.
+% - 'convention' is the convention used for the D-H parameters. Accepted
+%    values are 'standard' and 'modified'
+%
+% The first row of 'A' contains the joint offsets. More specifically,
+% theta_i is the offset for the i-th joint.
 %
 % Type DQ_kinematics.(method,property) for specific help.
 % Ex.: help DQ_kinematics.fkm
@@ -28,19 +30,19 @@
 % See also DQ, DQ_KinematicController
 
 % (C) Copyright 2015 DQ Robotics Developers
-% 
+%
 % This file is part of DQ Robotics.
-% 
+%
 %     DQ Robotics is free software: you can redistribute it and/or modify
 %     it under the terms of the GNU Lesser General Public License as published by
 %     the Free Software Foundation, either version 3 of the License, or
 %     (at your option) any later version.
-% 
+%
 %     DQ Robotics is distributed in the hope that it will be useful,
 %     but WITHOUT ANY WARRANTY; without even the implied warranty of
 %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %     GNU Lesser General Public License for more details.
-% 
+%
 %     You should have received a copy of the GNU Lesser General Public License
 %     along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
 %
@@ -48,6 +50,10 @@
 %
 % Contributors to this file:
 %     Bruno Vihena Adorno - adorno@ufmg.br
+
+% TODO: Remove the virtual joints. Instead of helping, they cause a lot of
+% confusion, specially among those trying to learn the library. The
+% affected methods are: FKM and Jacobian.
 
 classdef DQ_kinematics < handle
     properties
@@ -59,7 +65,9 @@ classdef DQ_kinematics < handle
         base;
         effector;
         
-        % Properties for interfacing with Robotics Toolbox  
+        % Properties for interfacing with Robotics Toolbox. Those are
+        % legacy properties and are likely to disappear in the near
+        % future.
         name;
         plotopt
         lineopt
@@ -67,22 +75,28 @@ classdef DQ_kinematics < handle
         robot_RT;
     end
     
-      properties (SetAccess = private)
-       
+    properties (SetAccess = private)
+        
         handle
+        %TODO: rename this property to avoid confusion with robot
+        %configuration
         q
-       
+        
     end
     
+    
+    %TODO: Re-evaluate the need of redefining these properties below as 
+    %      they are already declared in DQ.
     properties (Constant)
         % Given the jacobian matrix J that satisfies
         % vec(dot_x) = J * dot_theta, where x is a dual quaternion, the
         % following relation is established:
-        % vec(dot_x) = C8 * J * dot_theta
+        % vec(dot_x') = C8 * J * dot_theta, where x' is the conjugate of x.
         C8 = diag([1, -1, -1, -1, 1, -1, -1, -1]);
         % Given the jacobian matrix J that satisfies
-        % vec(dot_x) = J * dot_theta, where x is a quaternion, the following
-        % relation is established: vec(dot_x) = C8 * J * dot_theta
+        % vec(dot_r) = J * dot_theta, where r is a quaternion, the following
+        % relation is established: vec(dot_r') = C4 * J * dot_theta, where
+        % r' is the conjugate of r.
         C4 = diag([1, -1, -1, -1]);
     end
     
@@ -92,13 +106,13 @@ classdef DQ_kinematics < handle
                 error('Input: matrix whose columns contain the DH parameters')
             end
             
-            obj.n_dummy = 0; 
+            obj.n_dummy = 0;
             if size(A,1) == 5
-                %There are dummy joints                
+                %There are dummy joints
                 obj.dummy = A(5,:);
                 obj.n_dummy = sum(obj.dummy == 1);
                 
-            else                
+            else
                 obj.dummy = zeros(1,size(A,2));
             end
             
@@ -112,38 +126,35 @@ classdef DQ_kinematics < handle
             obj.effector = DQ(1); %Default effector's pose
             
             %Definitions for Robotics Toolbox
-            obj.name = sprintf('%f',rand(1));            
-           % obj.robot_RT = SerialLink(A(1:4,:)','name',obj.name);
-            
+            obj.name = sprintf('%f',rand(1));
+                        
             if nargin==1
-                obj.convention='standard';  
-           %      obj.robot_RT = SerialLink(A(1:4,:)','name',obj.name);
+                obj.convention='standard';
             else
                 obj.convention=type;
-               % obj.robot_RT = SerialLink(A(1:4,:)','name',obj.name,'mdh',1);
             end
-            % obj.robot_RT = SerialLink(A(1:4,:)','name',obj.name);
-             
-             
-             %For visualisation
-             obj.lineopt = {'Color', 'black', 'Linewidth', 4};
+            
+            %For visualisation
+            obj.lineopt = {'Color', 'black', 'Linewidth', 4};
             obj.shadowopt = {'Color', 0.7*[1 1 1], 'Linewidth', 3};
             obj.plotopt = {};
-           
+            
         end
         
         function set_base(obj,base)
             % dq.set_base(base) sets the pose of the robot's base
+            
             obj.base = DQ(base);
         end
         
         function set_effector(obj,effector)
             % dq.set_effector(effector) sets the pose of the effector
+            
             obj.effector = DQ(effector);
         end
-            
         
-       
+        
+        
         
         function q = raw_fkm(obj,theta, ith)
             %   dq = raw_fkm(theta) calculates the forward kinematic model and
@@ -168,7 +179,8 @@ classdef DQ_kinematics < handle
             j = 0;
             for i=1:n
                 if obj.dummy(i) == 1
-                    q = q*dh2dq(obj,0,i); %The offset is taken into account inside the method dh2dq
+                    %The offset is taken into account inside the method dh2dq
+                    q = q*dh2dq(obj,0,i); 
                     j = j + 1;
                 else
                     q = q*dh2dq(obj,theta(i-j),i);
@@ -183,7 +195,7 @@ classdef DQ_kinematics < handle
             %   displacement due to the base's and effector's poses.
             %
             %   theta is the vector of joint variables
-            %   
+            %
             %   dq = fkm(theta, ith) calculates the FKM up to the ith link.
             
             if nargin == 3
@@ -253,38 +265,67 @@ classdef DQ_kinematics < handle
             p(8)=q(4)*q(8)-q(3)*q(7)-q(2)*q(6)+q(1)*q(5);
         end
         
-        
-        
-        function J = jacobian(obj,theta)
-            % J = jacobian(theta) returns the dual quaternion Jacobian, where
-            % theta is the vector of joint variables
-            q_effector = obj.raw_fkm(theta);
+        function J = raw_jacobian(obj,theta,ith)
+            % J = raw_jacobian(theta) returns the first ith columns of the
+            % Jacobian that satisfies vec(x_dot) = J * theta_dot, where
+            % x = fkm(theta) and theta is the vector of joint variables.
+            % This function does not take into account any base or
+            % end-effector displacements and should be used mostly
+            % internally in DQ_kinematics            
             
-            q = DQ(1);
-            J= zeros(8,obj.links-obj.n_dummy);
-            ith=0;
-            
-            for i = 0:obj.links-1
-                
-                
-                    %Use the standard DH convention
-                    if strcmp(obj.convention,'standard')
-                        z = obj.get_z(q.q);
-                    else %Use the modified DH convention
-                        w=DQ([0,0,-sin(obj.alpha(i+1)),cos(obj.alpha(i+1)),0,0,-obj.a(i+1)*cos(obj.alpha(i+1)),-obj.a(i+1)*sin(obj.alpha(i+1)) ] );
-                        z =0.5*q*w*q';
-                    end
-                    
-                    if ~obj.dummy(i+1)
-                        q = q*obj.dh2dq(theta(ith+1),i+1);
-                        j = z * q_effector;
-                        J(:,ith+1) = j.q;
-                        ith = ith+1;                    
-                    else
-                        q = q*obj.dh2dq(0,i+1); %Dummy joints don't contribute to the Jacobian
-                    end
+            if nargin == 3
+                n = ith;
+            else
+                n = obj.links;
             end
-            J = hamiplus8(obj.base)*haminus8(obj.effector)*J; %Takes the base's displacement into account
+            
+            x_effector = obj.raw_fkm(theta);            
+            x = DQ(1);
+            J= zeros(8,n-obj.n_dummy);
+            jth=0;
+            
+            for i = 0:n-1                                
+                % Use the standard DH convention
+                if strcmp(obj.convention,'standard')
+                    z = obj.get_z(x.q);
+                else % Use the modified DH convention
+                    w=DQ([0,0,-sin(obj.alpha(i+1)),cos(obj.alpha(i+1)),0,0,-obj.a(i+1)*cos(obj.alpha(i+1)),-obj.a(i+1)*sin(obj.alpha(i+1)) ] );
+                    z =0.5*x*w*x';
+                end
+                
+                if ~obj.dummy(i+1)
+                    x = x*obj.dh2dq(theta(jth+1),i+1);
+                    j = z * x_effector;
+                    J(:,jth+1) = j.q;
+                    jth = jth+1;
+                else
+                    % Dummy joints don't contribute to the Jacobian
+                    x = x*obj.dh2dq(0,i+1); 
+                end
+            end
+        end
+                   
+        
+        function J = jacobian(obj, theta, ith)
+            % J = jacobian(theta) returns the Jacobian that satisfies
+            % vec(x_dot) = J * theta_dot, where x = fkm(theta) and 
+            % theta is the vector of joint variables. It takes into account
+            % both base and end-effector displacements (their default
+            % values are 1).
+           
+            if nargin == 3 && ith < obj.links
+                % If the Jacobian is not related to the mapping between the
+                % end-effector velocities and the joint velocities, it takes 
+                % into account only the base displacement               
+                J = hamiplus8(obj.base)*obj.raw_jacobian(theta, ith); 
+            else
+                % Otherwise, it the Jacobian is related to the
+                % end-effector velocity, it takes into account both base
+                % and end-effector (constant) displacements.
+                J = hamiplus8(obj.base)*haminus8(obj.effector)*obj.raw_jacobian(theta);
+            end
+            
+            
         end
         
     end
