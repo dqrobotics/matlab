511be2cff48c8c3af97d341fd2ccc6cf90d38bc6
Bruno Vilhena Adorno <adorno@ufmg.br>
2019-03-27 18:50:31 -0300

    Improved the documentation and fixed "bug" #6.

--- examples/AX18_kinematic_control.m
+++ examples/AX18_kinematic_control.m
@@ -3,25 +3,34 @@ clear all;
 clear classes;
 clc;
 
-%Create a new DQ_kinematics object with the AX18 arm standard Denavit-Hartenberg parameters           
+% Create a new DQ_kinematics object with the AX18 arm standard 
+% Denavit-Hartenberg parameters           
 ax18 = DQ_AX18;
 
-%Initial configuration
-theta=[0 0 0 0 0]'; 
+% Initial configuration
+theta=[0 0 0 0 0]';
 
-%Configuring the end-effector:
-ax18.set_effector(cos(pi/4)+DQ.i*sin(pi/4))
+% Configuring the end-effector:
+ax18.set_effector(cos(pi/4)+DQ.i*sin(pi/4));
 
-p = translation(ax18.fkm(theta));
-pd = translation(ax18.fkm([pi/4 -pi/4 pi/2 0 pi/4]));
+% Generate a desired pose base on final joint angles. In real applications,
+% usually we don't have the desired joint configurations.
+xd = ax18.fkm([pi/4 -pi/4 pi/2 0 pi/4]);
 
-error = pd - p;
-while norm(vec4(error)) > 0.1
-    x = ax18.fkm(theta);
-    p = translation(x);
-    Jp = ax18.jacobp(ax18.jacobian(theta),x);
-    error = pd - p;
-    theta = theta + pinv(Jp)*0.1*vec4(error);
-    plot(ax18,theta);
+x = ax18.fkm(theta);
+
+% Let us plot the desired pose
+plot(xd, 'scale', 0.1);
+
+% This is a very simple way for calculating the error, but it has some
+% problems. For instance, it does not respect the topology of the unit dual
+% quaternion space.
+error = xd - x;
+while norm(vec4(error)) > 0.01
+    x = ax18.fkm(theta);   
+    J = ax18.jacobian(theta);
+    error = xd - x;    
+    theta = theta + pinv(J)*0.05*vec8(error);
+    plot(ax18,theta);   
     drawnow;
 end
