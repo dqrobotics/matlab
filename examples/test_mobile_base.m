8e31e063c44b8e137c0e72ebfc1f1e85b17bc6ae
Bruno Vilhena Adorno <adorno@ufmg.br>
2019-03-28 18:39:50 -0300

    Added an example, still under development, to test the class DQ_MobileBase.

---
+++ examples/test_mobile_base.m
@@ -0,0 +1,36 @@
+% Simple example to test the DQ_MobileBase class. Still under development.
+
+% (C) Copyright 2019 DQ Robotics Developers
+%
+% This file is part of DQ Robotics.
+%
+%     DQ Robotics is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU Lesser General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+%
+%     DQ Robotics is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU Lesser General Public License for more details.
+%
+%     You should have received a copy of the GNU Lesser General Public License
+%     along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
+%
+% DQ Robotics website: dqrobotics.github.io
+%
+% Contributors to this file:
+%     Bruno Vihena Adorno - adorno@ufmg.br
+
+clear all;
+close all;
+clc;
+
+robot_struct.robot_type = 'holonomic';
+
+mobile_base = DQ_MobileBase(robot_struct);
+
+mobile_base.this_robot_type
+q = [1,1,pi];
+pose = mobile_base.fkm(q);
+
