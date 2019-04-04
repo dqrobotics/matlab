c8b15279b4789275b6d7dbe1e709da24c0158c74
Bruno Vilhena Adorno <adorno@ufmg.br>
2019-04-04 14:14:59 -0300

    Made adaptation to take into account the addition of reference frames and base frames.

--- DQ_MobileBase/DQ_WholeBody.m
+++ DQ_MobileBase/DQ_WholeBody.m
@@ -1,5 +1,5 @@
 classdef DQ_WholeBody < handle
-    properties (Access = protected)
+    properties %(Access = protected)
         chain;
     end
     
@@ -9,26 +9,27 @@ classdef DQ_WholeBody < handle
             obj.chain{1} = robot;
         end
         
-        % add(robot) adds a robot to the end of the serial kinematic chain
         function add(obj, robot)
+            % add(robot) adds a robot to the end of the serial kinematic chain
             % TODO: test if robot is an instance of DQ_Kinematics
             len = length(obj.chain);
             obj.chain{len + 1} = robot;
-        end
-        
+        end        
+       
+        function x = fkm(obj,q,ith)
         % x = fkm(q) receives the configuration vector q of the whole
         % kinematic chain and returns the pose of the last frame.
         % x = fkm(q, ith) calculates the forward kinematics up to the ith
         % kinematic chain.
-        function x = fkm(obj,q,ith)
-            x = DQ(1);
-            j = 1; % first configuration vector (q1)            
             
             if nargin > 2
                 n = ith;
             else
                 n = length(obj.chain);
             end
+            
+            x = DQ(1);
+            j = 1; % first configuration vector (q1)      
             
             % Iterate over the chain
             for i = 1:n
@@ -44,21 +45,23 @@ classdef DQ_WholeBody < handle
         end
         
         function plot(obj,q)
-            j = 1;
+            
+            current_base_frame = obj.chain{1}.base_frame;
+            n_links = obj.chain{1}.n_links;
+            plot(obj.chain{1},q(1:n_links),'cylinder',[0, 0, 0]);            
+            
+            j = n_links + 1;
+            
             % Iterate over the chain
-            for i = 1:length(obj.chain)
+            for i = 2:length(obj.chain)
                 % Replace n_links by dimension_configuration_space
                 dim = obj.chain{i}.n_links;
                 qi = q(j : j + dim - 1);
-                j = j + dim;
-                old_base = obj.chain{i}.base;
-               
-                if i > 1
-                    obj.chain{i}.set_base(obj.fkm(q,i-1));
-                end
-                plot(obj.chain{i},qi);
+                j = j + dim;               
                 
-                obj.chain{i}.set_base (old_base);
+                current_base_frame = obj.chain{1}.base_frame*obj.fkm(q,i-1);
+                obj.chain{i}.set_base_frame(current_base_frame);                    
+                plot(obj.chain{i},qi,'cylinder',[0,i*0.2,0], 'nobase');         
             end            
         end
         
--- examples/robot_serialization.m
+++ examples/robot_serialization.m
@@ -2,23 +2,36 @@ clear all;
 close all;
 clc;
 
+include_namespace_dq;
+
+base = DQ_HolonomicBase;
 arm1 = DQ_KUKA;
+arm1.name = 'base';
 arm2 = DQ_KUKA;
+arm3 = DQ_KUKA;
+arm4 = DQ_KUKA;
 
 % Initializes the robot with arm1
 robot = DQ_WholeBody(arm1);
+%robot.add(arm1)
+
 % Add another independent arm serially coupled to
 robot.add(arm2);
+robot.add(arm3);
+robot.add(arm4)
 
-q = zeros(14,1);
+q = zeros(28,1);
 % robot.fkm(q), where q = [q1; q2], is equivalent to arm1.fkm(q1)*arm2.fkm(q2)
 x = robot.fkm(q);
 % Get the whole-body Jacobian
 %J = robot.pose_jacobian(q);
 
 % Plot the whole kinematic chain
+plot(DQ(1));
+hold on;
+plot(arm1.base_frame);
 plot(robot,q);
-axis([-2,2,-2,2,-2,3]);
+axis([-10,1,0,2,0,5]);
 hold on;
 
 for i=0:0.1:pi/2
