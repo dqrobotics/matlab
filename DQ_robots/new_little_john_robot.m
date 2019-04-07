5d8c197082130754dfed516a894c7f003c8fc251
Bruno Vilhena Adorno <adorno@ufmg.br>
2019-04-07 01:55:59 -0300

    Added the iRoomba Create robot and MACRO's mobile manipulator Little John.

---
+++ DQ_robots/new_little_john_robot.m
@@ -0,0 +1,39 @@
+% (C) Copyright 2011-2019 DQ Robotics Developers
+% 
+% This file is part of DQ Robotics.
+% 
+%     DQ Robotics is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU Lesser General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     DQ Robotics is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU Lesser General Public License for more details.
+% 
+%     You should have received a copy of the GNU Lesser General Public License
+%     along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
+%
+% DQ Robotics website: dqrobotics.sourceforge.net
+%
+% Contributors to this file:
+%     Bruno Vihena Adorno - adorno@ufmg.br
+
+
+function robot = new_little_john_robot()
+    % Mobile manipulator composed of a nonholonomic mobile base (Create)
+    % serially coupled to a 5-DOF arm (made of AX18 servos)
+    arm = DQ_AX18();
+    base = new_create_robot();
+    
+    include_namespace_dq
+    
+    % Changing the reference due to the mobile base height
+    x = 1 + E_*(1/2)*170e-3*k_;
+    
+    base.set_frame_displacement(x);
+    
+    robot = DQ_WholeBody(base);
+    robot.add(arm);
+end
