bfaf507b23517461c3a5f3bd8633f836ea7c60b8
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2021-12-27 17:50:43 -0300

    [DQ_SerialManipulatorMDH] The dh_matrix_ property is now protected

--- robot_modeling/DQ_SerialManipulatorMDH.m
+++ robot_modeling/DQ_SerialManipulatorMDH.m
@@ -57,7 +57,7 @@ classdef DQ_SerialManipulatorMDH < DQ_SerialManipulator
         %theta,d,a,alpha;
     %end
     
-    properties %(Access = protected)
+    properties (Access = protected)
         dh_matrix_;
        
     end
