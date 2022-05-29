3e1d4eb5357e625ef932301ae03f8cae1443748f
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2022-05-29 19:45:56 +0900

    [BarretWamArmRobot.m] Updated the type of joint.

--- robots/BarrettWamArmRobot.m
+++ robots/BarrettWamArmRobot.m
@@ -29,7 +29,7 @@ classdef BarrettWamArmRobot
             wam_DH_d = [0, 0, 0.55, 0, 0.3, 0, 0.0609];
             wam_DH_a = [0, 0, 0.045, -0.045, 0, 0, 0];
             wam_DH_alpha =  pi*[-0.5, 0.5, -0.5, 0.5, -0.5, 0.5, 0];
-            wam_DH_type  = repmat(DQ_SerialManipulatorDH.JOINT_ROTATIONAL,1,7);
+            wam_DH_type  = repmat(DQ_SerialManipulatorDH.ROTATIONAL,1,7);
             wam_DH_matrix = [wam_DH_theta;
                               wam_DH_d;
                               wam_DH_a;
